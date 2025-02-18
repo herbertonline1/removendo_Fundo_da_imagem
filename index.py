db60aba39bcad1f0b24cd419d918734a13d1e6a2
Herbert Santos <herbertonline1@hotmail.com>
2025-02-18 18:48:42 -0300

    removendo fundo de imagem

---
+++ index.py
@@ -0,0 +1,20 @@
+# Importando a biblioteca rembg
+# OBS: Precisa instalar o rembg "pip install rembg"
+from rembg import remove
+
+from PIL import Image
+
+
+# Caminho onde a foto esta salva
+caminho_imagem = r'C:\Users\herbert.santos\Desktop\Nova pasta\download.jpg'
+#Caminho onde a foto vai ser salva, OBS: Mudar o nome e a extesão da foto para .png
+caminho_imagem_com_bg_removido = r'C:\Users\herbert.santos\Desktop\Nova pasta\download_sem_bg.png'
+# Aqui vamos abrir a foto 
+imagem_arquivo = Image.open(caminho_imagem) 
+# Aqui vamos remover o fundo da foto
+imagem_arquivo_sem_bg = remove(imagem_arquivo)
+#Aqui vamos salvar a foto que foi removida o fundo
+imagem_arquivo_sem_bg.save(caminho_imagem_com_bg_removido)
+
+
+
